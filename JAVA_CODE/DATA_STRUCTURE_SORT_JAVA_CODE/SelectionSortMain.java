e280fa174d02c389fbdaacc594a5f8b8a243fadd
averma-akash <averma.akash@gmail.com>
2022-01-12 20:22:32 +0530

    added

---
+++ JAVA_CODE/DATA_STRUCTURE_SORT_JAVA_CODE/SelectionSortMain.java
@@ -0,0 +1,38 @@
+package Sorting;
+
+import java.util.Arrays;
+
+/*
+ * Find the minimum element in the list.
+Swap minimum element with current element.
+Repeat the whole process until array is fully sorted.
+ */
+public class SelectionSortMain {
+
+	public static int[] selectionSort(int[] arr) {
+
+		for (int i = 0; i < arr.length - 1; i++) {
+			int index = i;
+			for (int j = i + 1; j < arr.length; j++)
+				if (arr[j] < arr[index])
+					index = j;
+
+			int smallerNumber = arr[index];
+			arr[index] = arr[i];
+			arr[i] = smallerNumber;
+		}
+		return arr;
+	}
+
+	public static void main(String a[]) {
+
+		int[] arr = { 40, 10, -30, 45, 39, 32 };
+		System.out.println("Before Sorting : ");
+		System.out.println(Arrays.toString(arr));
+		arr = selectionSort(arr);
+		System.out.println("===================");
+		System.out.println("After Sorting : ");
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
