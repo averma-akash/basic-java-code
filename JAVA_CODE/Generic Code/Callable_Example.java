f0db9c032fc220427fed32b4ed0a4b3d25485f53
averma-akash <averma.akash@gmail.com>
2022-02-23 20:59:43 +0530

    edited

--- JAVA_CODE/Generic Code/Callable_Example.java
+++ JAVA_CODE/Generic Code/Callable_Example.java
@@ -1,29 +1,23 @@
-// Java Program to illustrate Callable interface
-
-// Importing classes from java.util package
 import java.util.Random;
 import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
-
-// Class
-// Implementing the Callable interface
-class CallableExample implements Callable {
-	
-// Main driver method
-public static void main(String args[])
+  
+class CallableExample implements Callable
 {
-	
-	// public Object call() throws Exception
-	
-		
-	Random generator = new Random();
-
-	Integer randomNumber = generator.nextInt(5);
-
-		
-	Thread.sleep(randomNumber * 1000);
-
-	return randomNumber;
-	}
-	
+  
+    public Object call() throws Exception
+    {
+        // Create random number generator
+        Random generator = new Random();
+  
+        Integer randomNumber = generator.nextInt(5);
+  
+        // To simulate a heavy computation,
+        // we delay the thread for some random time
+        Thread.sleep(randomNumber * 1000);
+  
+        return randomNumber;
+    }
 }
+
+//Here is the code for an example Callable, which will return a random number after a delay of around 0 – 4 seconds.
