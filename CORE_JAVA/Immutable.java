7cbcda5dd4543b1d69e9f4e433970373a9461312
averma-akash <averma.akash@gmail.com>
2022-03-21 10:34:26 +0530

    Update Immutable.java

--- CORE_JAVA/Immutable.java
+++ CORE_JAVA/Immutable.java
@@ -7,30 +7,74 @@ the getter method should return the copy of class members
 class members are only initialized using constructor
 
 */
-
-// class is declared final
-final class Immutable {
-
-  // private class members
-  private String name;
-  private int date;
-
-  Immutable(String name, int date) {
-
-    // class members are initialized using constructor
-    this.name = name;
-    this.date = date;
+import java.util.Date;
+ 
+/**
+* Always remember that your instance variables will be either mutable or immutable.
+* Identify them and return new objects with copied content for all mutable objects.
+* Immutable variables can be returned safely without extra effort.
+* */
+public final class ImmutableClass
+{
+ 
+  /**
+  * Integer class is immutable as it does not provide any setter to change its content
+  * */
+  private final Integer immutableField1;
+ 
+  /**
+  * String class is immutable as it also does not provide setter to change its content
+  * */
+  private final String immutableField2;
+ 
+  /**
+  * Date class is mutable as it provide setters to change various date/time parts
+  * */
+  private final Date mutableField;
+ 
+  //Default private constructor will ensure no unplanned construction of class
+  private ImmutableClass(Integer fld1, String fld2, Date date)
+  {
+    this.immutableField1 = fld1;
+    this.immutableField2 = fld2;
+    this.mutableField = new Date(date.getTime());
   }
-
-  // getter method returns the copy of class members
-  public String getName() {
-    return name;
+ 
+  //Factory method to store object creation logic in single place
+  public static ImmutableClass createNewInstance(Integer fld1, String fld2, Date date)
+  {
+    return new ImmutableClass(fld1, fld2, date);
   }
-
-  public int getDate() {
-    return date;
+ 
+  //Provide no setter methods
+ 
+  /**
+  * Integer class is immutable so we can return the instance variable as it is
+  * */
+  public Integer getImmutableField1() {
+    return immutableField1;
   }
-
+ 
+  /**
+  * String class is also immutable so we can return the instance variable as it is
+  * */
+  public String getImmutableField2() {
+    return immutableField2;
+  }
+ 
+  /**
+  * Date class is mutable so we need a little care here.
+  * We should not return the reference of original instance variable.
+  * Instead a new Date object, with content copied to it, should be returned.
+  * */
+  public Date getMutableField() {
+    return new Date(mutableField.getTime());
+  }
+ 
+  @Override
+  public String toString() {
+    return immutableField1 +" - "+ immutableField2 +" - "+ mutableField;
+  }
 }
 
 class Main {
@@ -41,5 +85,24 @@ class Main {
 
     System.out.println("Name: " + obj.getName());
     System.out.println("Date: " + obj.getDate());
+  }
+}
+
+
+class TestMain
+{
+  public static void main(String[] args)
+  {
+    ImmutableClass im = ImmutableClass.createNewInstance(100,"test", new Date());
+    System.out.println(im);
+    tryModification(im.getImmutableField1(),im.getImmutableField2(),im.getMutableField());
+    System.out.println(im);
+  }
+ 
+  private static void tryModification(Integer immutableField1, String immutableField2, Date mutableField)
+  {
+    immutableField1 = 10000;
+    immutableField2 = "test changed";
+    mutableField.setDate(10);
   }
 }
