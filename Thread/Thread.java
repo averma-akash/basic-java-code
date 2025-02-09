3c8da6805dfc624be77c163e31e3d329703ef63c
averma-akash <70502639+averma-akash@users.noreply.github.com>
2025-02-09 23:50:23 +0530

    Update Thread.java

--- Thread/Thread.java
+++ Thread/Thread.java
@@ -3,10 +3,14 @@
 ╚════════╝
 a thread is a part of an application that shares a common memory with other threads of the same application.
 
-******* Daemon thread ******* 
+╔═══════════════════════════════╗
+║ ******* Daemon thread ******* ║
+╚═══════════════════════════════╝
 in Java is a low-priority thread that runs in the background to perform tasks such as garbage collection. Daemon thread in Java is also a service provider thread that provides services to the user thread. Its life depends on user threads i.e. when all the user threads die, JVM terminates this thread automatically.
 
-2. ******* Different state of Thread ? *******
+╔═════════════════════════════════════════════╗
+║ ******* Different state of Thread ? ******* ║
+╚═════════════════════════════════════════════╝
 
 NEW — a new Thread instance that was not yet started via Thread.start()
 RUNNABLE — a running thread. It is called runnable because at any given time it could be either running or waiting for the next quantum of time from the thread scheduler. A NEW thread enters the RUNNABLE state when you call Thread.start() on it
@@ -15,30 +19,44 @@ WAITING — a thread enters this state if it waits for another thread to perform
 TIMED_WAITING — same as the above, but a thread enters this state after calling timed versions of Thread.sleep(), Object.wait(), Thread.join() and some other methods
 TERMINATED — a thread has completed the execution of its Runnable.run() method and terminated
 
-****** What is multithreading? ******
+╔═══════════════════════════════════════╗
+║ ****** What is multithreading? ****** ║
+╚═══════════════════════════════════════╝
 
 Multithreading is a process of executing multiple threads simultaneously. Multithreading is used to obtain the multitasking. It consumes less memory and gives the fast and efficient performance.
 
-****** wait() ******
+╔══════════════════════╗
+║ ****** wait() ****** ║
+╚══════════════════════╝
 
 The wait() method is provided by the Object class in Java. This method is used for inter-thread communication in Java. The java.lang.Object.wait() is used to pause the current thread, and wait until another thread does not call the notify() or notifyAll() method. public final void wait()
 
-****** join() ******
+╔══════════════════════╗
+║ ****** join() ****** ║
+╚══════════════════════╝
 
 The join() method waits for a thread to die. In other words, it causes the currently running threads to stop executing until the thread it joins with completes its task. 
 
-**** sleep() ****
+╔═══════════════════════╗
+║ ****** sleep() ****** ║
+╚═══════════════════════╝
 The sleep() method in java is used to block a thread for a particular time, which means it pause the execution of a thread for a specific time. 
 
-***** Is it possible to start a thread twice? ******
+╔═══════════════════════╗
+║ start a thread twice? ║
+╚═══════════════════════╝
 
 No, we cannot restart the thread, as once a thread started and executed, it goes to the Dead state. Therefore, if we try to start a thread twice, it will give a runtimeException "java.lang.IllegalThreadStateException".
 
-***** Can we call the run() method instead of start()? ******
+╔═══════════════════════════════════════════╗
+║ call the run() method instead of start()? ║
+╚═══════════════════════════════════════════╝
 
 Yes, calling run() method directly is valid, but it will not work as a thread instead it will work as a normal object. There will not be context-switching between the threads. When we call the start() method, it internally calls the run() method, which creates a new stack for a thread while directly calling the run() will not create a new stack.
 
-***** What is shutdown hook? *****
+╔════════════════════════╗
+║ What is shutdown hook? ║
+╚════════════════════════╝
 
 The shutdown hook is a thread that is invoked implicitly before JVM shuts down. So we can use it to perform clean up the resource or save the state when JVM shuts down normally or abruptly. We can add shutdown hook by using the following method:*/
 
@@ -46,7 +64,9 @@ public void addShutdownHook(Thread hook){}
 Runtime r=Runtime.getRuntime();  
 r.addShutdownHook(new MyThread());  
 
-/*4. ***** Broken Barrier ******
+╔════════════════╗
+║ Broken Barrier ║
+╚════════════════╝
 
 A barrier breaks when any of the waiting thread leaves the barrier. This happens when one or more waiting thread is interrupted or when the waiting time is completed because the thread called the await() methods with a timeout as follows:*/
 
@@ -77,7 +97,9 @@ Thread t = new Thread(new Runnable(){
  });
  t.start(); 
 
-/********** Thread Pool **********
+╔═════════════╗
+║ Thread Pool ║
+╚═════════════╝
 
 A Thread pool in Java is a group of worker threads that can be reused many times and assigned jobs.
 
